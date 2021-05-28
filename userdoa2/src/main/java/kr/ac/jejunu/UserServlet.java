28de8fa9dcbf5dd033289aa61755df14f580770c
wonhojin <ghgud0748@gmail.com>
2021-05-28 15:23:53 +0900

    05/14 수업
    05/14 수업

--- userdoa2/src/main/java/kr/ac/jejunu/UserServlet.java
+++ userdoa2/src/main/java/kr/ac/jejunu/UserServlet.java
@@ -1,21 +1,40 @@
 package kr.ac.jejunu;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import javax.servlet.*;
 import java.io.IOException;
 
 public class UserServlet extends GenericServlet {
+    private UserDao userDao;
     @Override
     public void destroy() {
         System.out.println("*************** destroy *****************");
+        super.destroy();
     }
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         System.out.println("*************** init *****************");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext("kr.ac.jejunu.user");
+        this.userDao = applicationContext.getBean("userDao", UserDao.class);
+        super.init(config);
     }
 
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+        User user = userDao.get(2);
+        StringBuffer stringBuffer = new StringBuffer();
+
         System.out.println("*************** service *****************");
+
+        stringBuffer.append("<html>");
+        stringBuffer.append("<h1>");
+        stringBuffer.append(String.format("Hello %s!!!"user.getName()));
+        stringBuffer.append("</h1>");
+        stringBuffer.append("</html>");
+        res.getWriter().println(StringBuffer.toString());
     }
 }
